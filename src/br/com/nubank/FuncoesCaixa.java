8715590c4a5101b366d10982743d8d61e22ee1d5
otacrazy2123 <andre.sousa.tec@gmail.com>
2020-10-10 19:33:03 -0300

    Initial commit

---
+++ src/br/com/nubank/FuncoesCaixa.java
@@ -0,0 +1,5 @@
+package br.com.nubank;
+
+public class FuncoesCaixa {
+
+}
---
+++ src/br/com/nubank/app.java
@@ -0,0 +1,10 @@
+package br.com.nubank;
+
+public class app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
